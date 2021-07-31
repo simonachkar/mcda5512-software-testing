473f6758ecdbd7916948622c845514e8f36cd99a
Simon Achkar <simon@simonachkar.com>
2021-07-30 23:45:58 -0300

    [03]: more test examples + finished.txt

--- 03-jest/functions.js
+++ 03-jest/functions.js
@@ -1,6 +1,11 @@
 const functions = {
     add: (a, b) => { return a + b },
     isNull: () => { return null },
+
+    getUser: () => {
+        const user = { firstName: 'Joe', lastName: 'Doe' }
+        return user
+    }
 }
 
 module.exports = functions
