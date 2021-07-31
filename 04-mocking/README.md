b1a80810f77e45a92d145d4170012d8d5606ce40
Simon Achkar <simon@simonachkar.com>
2021-07-31 01:05:12 -0300

    [04]: more tests + README

---
+++ 04-mocking/README.md
@@ -0,0 +1,15 @@
+# Mocking
+
+An example about Mocking
+
+## Getting started
+Make sure you're in `04-mocking` directory, and then run:
+```
+npm install
+```
+
+## Running tests
+To test make sure you complete the code `fight.test.js` (you can refer to `_finished.txt`). Then run:
+```
+npm run test
+```
--- 04-mocking/fight.test.js
+++ 04-mocking/fight.test.js
@@ -1,12 +1,20 @@
 const fight = require("./fight");
 const utils = require("./utils")
 
-// Mock utils module
+// Mock ./utils and make getWinner always return boxer1
 // ...
 
 test('Winner is Elon Musk', () => {
-    const winner = fight('Jeff Bezos', 'Elon Musk')
+    const winner = fight('Elon Musk', 'Jeff Bezos')
     expect(winner).toBe('Elon Musk')
 
+    // Test getWinner
+    // - been called
+    // - been called with the right args
+    // - been called 5 time
+
+    // ... 
+
     // cleanup
+    // ...
 })
